5441ac42d1032e67f9809d13e1432244b4d7914f
SoloWolf <danielaguerreberry@live.com.ar>
2023-02-06 08:25:15 -0300

    1

---
+++ Assets/Scripts/Colliders/CollidersCharecterMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollidersCharecterMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float giroSpeed;
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 direction = new(0, 0, vertical);
+
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * vertical);
+        transform.Rotate(Vector3.up, horizontal * giroSpeed * Time.deltaTime);
+
+    }
+}
